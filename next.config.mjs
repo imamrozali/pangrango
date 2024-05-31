86ff1cf093797c60a52b3c3451b8722e0f764aa2
imam.rozali <imam.rozali@bvarta.com>
2024-05-31 09:12:05 +0700

    update

--- next.config.mjs
+++ next.config.mjs
@@ -4,59 +4,12 @@ import withPWA from "next-pwa";
 
 const isProduction = process.env.NODE_ENV === "production";
 
-const securityHeaders = [
-  {
-    key: "X-DNS-Prefetch-Control",
-    value: "on",
-  },
-  {
-    key: "Strict-Transport-Security",
-    value: "max-age=31536000; includeSubDomains; preload",
-  },
-  {
-    key: "X-XSS-Protection",
-    value: "1; mode=block",
-  },
-  {
-    key: "X-Frame-Options",
-    value: "SAMEORIGIN",
-  },
-  {
-    key: "Permissions-Policy",
-    value: "microphone=(), browsing-topics=()",
-  },
-  {
-    key: "X-Content-Type-Options",
-    value: "nosniff",
-  },
-  {
-    key: "Referrer-Policy",
-    value: "origin-when-cross-origin",
-  },
-];
-
 const globalConfig = {
-  // output: "export",
-  // basePath: "/pangrango",
-  // reactStrictMode: true,
-  // images: {
-  //   unoptimized: true,
-  // },
-  // swcMinify: false,
-  // poweredByHeader: false,
-  // devIndicators: {
-  //   buildActivity: false,
-  // },
-  env: {
-    APP_MODE: process.env.APP_MODE,
-  },
-  async headers() {
-    return [
-      {
-        source: "/:path*",
-        headers: securityHeaders,
-      },
-    ];
+  output: "export",
+  basePath: "/pangrango",
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
   },
 };
 
@@ -64,12 +17,7 @@ const provideConfig = withPWA({
   disable: isProduction,
   dest: "public",
   register: true,
-  output: "export",
-  basePath: "/pangrango",
-  reactStrictMode: true,
-  images: {
-    unoptimized: true,
-  },
+  buildExcludes: [/app-build-manifest.json$/],
 })(globalConfig);
 
 export default provideConfig;
